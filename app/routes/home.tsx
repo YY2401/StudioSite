8d794f9857725a5df5fad16ed10418d17dcf72c2
YY2401 <106874147+YY2401@users.noreply.github.com>
2025-07-19 11:56:06 +0800

    docs: For router to read rename home page file

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import NavBar from "../components/Navbar";
 import Carousel from "~/components/Carousel";
 import IndexIntro from "~/components/IndexIntro";
-import Store from "~/components/Store";
-import { CartProvider } from "~/components/CartContext";
 export default function Home() {
   return (
     <div>
